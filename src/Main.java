09aa3c91b038cafeff731eefa33cd63a5255400e
Sirikunya <fill9029f@gmail.com>
2018-10-31 18:42:58 +0700

    main

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String id;
+		String name;
+		Double salary;
+		Double sales;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Input Id Employee : ");
+		id = sc.nextLine();
+		System.out.print("Input Name Employee : ");
+		name = sc.nextLine();
+		System.out.print("Input Salary Employee: ");
+		salary = sc.nextDouble();
+		System.out.print("Input Sals : ");
+		sales=sc.nextDouble();
+		
+	}
+
+}
